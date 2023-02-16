fd661ebf388b4f65df26f8ae4e1e11cd66e99579
sakil-ahmed <sakilahmed1495@gmail.com>
2023-02-16 12:23:18 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.sendFile(__dirname + "/minimog/data.js");
+});
+
+app.listen(3000);
