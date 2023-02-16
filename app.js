52294a4bfdf93ec2ed84a46cee83fa941b797448
sakil-ahmed <sakilahmed1495@gmail.com>
2023-02-16 14:52:56 +0600

    update

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-app.get("/products", (req, res) => {
+app.get("/minimog/products", (req, res) => {
   res.sendFile(__dirname + "/minimog/db.json");
+});
+app.get("/voxo/products", (req, res) => {
+  res.sendFile(__dirname + "/voxo/db.json");
 });
 
 app.listen(3000);
